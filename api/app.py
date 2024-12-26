7f5bbae8ed9d8e4510c97019d15c5001dbbb9049
vkorostylev <vkorostylev@rutube.ru>
2024-12-26 23:04:34 +0300

    Some fix

--- api/app.py
+++ api/app.py
@@ -1,19 +1,17 @@
-# app.py
 from fastapi import FastAPI
 from pydantic import BaseModel
-from transformers import pipeline
 
 app = FastAPI()
-llm = pipeline("text-generation", model="gpt2")
+# llm = pipeline("text-generation", model="gpt2")
 
 
 class PromptRequest(BaseModel):
     prompt: str
 
+
 @app.post("/generate")
 async def generate_text(request: PromptRequest):
     prompt = request.prompt
-    response = llm(prompt, max_length=512, num_return_sequences=1)
+    # response = llm(prompt, max_length=512, num_return_sequences=1)
 
-    return {"generated_text": response[0]["generated_text"]}
-
+    return {"generated_text": "generated_text"}
--- bot/bot.py
+++ bot/bot.py
@@ -1,34 +1,57 @@
-# bot/bot.py
-import os
+import redis
 import requests
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram.filters import CommandStart
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
-API_URL = "http://api:5000/generate"  # URL API контейнера
+from constants import (
+    API_URL,
+    TELEGRAM_TOKEN,
+    START_MESSAGE,
+    CLEAR_DIALOG_EVENT_NAME,
+    USERNAME_KEY,
+)
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")  # Токен для бота
+r = redis.Redis(host="redis")
 bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher()
 
-# Обработчик всех текстовых сообщений
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    greet_kb = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text=CLEAR_DIALOG_EVENT_NAME)],
+        ]
+    )
+    await message.answer(START_MESSAGE, reply_markup=greet_kb)
+
+
 @dp.message()
 async def handle_message(message: Message):
-    prompt = message.text
-    try:
-        response = requests.post(API_URL, json={'prompt': prompt})
-        response.raise_for_status()  # Проверка на успешность запроса
-        generated_text = response.json()
-        await message.answer(generated_text)
-    except requests.exceptions.RequestException as e:
-        await message.answer("Произошла ошибка при генерации ответа.")
-        print(f"Error in API request: {e}")
+    user_text = message.text
+    user_id = message.from_user.id
 
-# Основная функция для запуска бота
+    if user_text == CLEAR_DIALOG_EVENT_NAME:
+        await r.get(USERNAME_KEY.format(user_id=user_id))
+
+    else:
+        try:
+            response = requests.post(API_URL, json={"prompt": user_text})
+            response.raise_for_status()
+            generated_text = str(response.json())
+            await message.answer(generated_text)
+        except requests.exceptions.RequestException as e:
+            await message.answer("Произошла ошибка при генерации ответа.")
+            print(f"Error in API request: {e}")
+
+
 async def main():
-    dp.include_router(dp)  # Регистрация маршрутов
-    await bot.delete_webhook(drop_pending_updates=True)  # Очистка старых обновлений
-    await dp.start_polling(bot)  # Запуск бота в режиме polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import asyncio
+
     asyncio.run(main())
